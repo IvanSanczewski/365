e629a2cd9a8218e12f100ed6791b61087dc7fab6
Ivan Sanczewski <isanczewski@gmail.com>
2024-12-19 14:10:15 +0200

    Added css styles

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ import data from './mockup.json' assert { type: 'json' };
 
 const meta = document.getElementById('meta');
 const location = document.querySelector('.location');
+const year = document.querySelector('.year');
 
 
 function dispplayContent() {
-    console.log(location.innerHTML);
     data.forEach(item => {
-        console.log(location);
-        location = item.location
+        console.log(location, year);
+        // meta.createNodeElement('div')
+        // location.textContent = item.location
+        // location.textContent = item.year
     })
 }
 
