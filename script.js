b59bfa90b32f2a4e72731af6e453be5dc489563a
Ivan Sanczewski <isanczewski@gmail.com>
2024-12-20 14:14:05 +0200

    Add styles to meta & content

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ import data from './mockup.json' assert { type: 'json' };
 const meta = document.getElementById('meta').innerHTML;
 const caption = document.querySelector('.caption');
 
+console.log(meta);
+console.log(caption);
+
 
 function displayContent() {
-    for (const item in data) {  
+    for (const item in data) {
+        meta.createNodeElement('p');
         caption.textContent = `${data[item].location}, ${data[item].year}` 
     }
 }
